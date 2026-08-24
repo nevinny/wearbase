fe6cb8a7f2a915694e2037044d54c7717f18b00f
zyablik <nevinny@gmail.com>
2026-08-25 00:28:04 +0300

    feat(look-share): CTA ref/target passthrough in auth forms and one-time referral_event on registration

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Auth;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -13,7 +14,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('account_dashboard');
@@ -24,9 +25,13 @@ class LoginController extends AbstractController
             $lastUsername = '';
         }
 
+        $lookShareTarget = (string) $request->query->get('target', '');
+
         return $this->render('auth/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $authenticationUtils->getLastAuthenticationError(),
+            // CTA лендинга «Поделиться луком»: только безопасный относительный путь на /l/{token}
+            'look_share_target' => preg_match('#^/l/[0-9a-f]{64}$#', $lookShareTarget) === 1 ? $lookShareTarget : null,
         ]);
     }
 
